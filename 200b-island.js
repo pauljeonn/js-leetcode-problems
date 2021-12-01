46ee9dd723386cb205ec5590d64c4c71313d5e4f
Paul Jeon <pauljeonn@gmail.com>
2021-12-01 17:14:58 +0900

    200b - dfs recursion

---
+++ 200b-island.js
@@ -0,0 +1,55 @@
+// Faster approach - recursion
+// Warning: This modifies the input
+var numIslands = function (grid) {
+	let islandCount = 0;
+
+	const dfs = (x, y) => {
+		if (grid[x][y] === '1') {
+			grid[x][y] = '0';
+		} else {
+			// '0'을 만났을때는 리턴해서 resursion을 빠져나온다.
+			return;
+		}
+
+		// search above
+		if (x > 0) {
+			dfs(x - 1, y);
+		}
+
+		// search below
+		if (x < grid.length - 1) {
+			dfs(x + 1, y);
+		}
+
+		// search left
+		if (y > 0) {
+			dfs(x, y - 1);
+		}
+
+		// search right
+		if (y < grid[x].length - 1) {
+			dfs(x, y + 1);
+		}
+	};
+
+	for (let i = 0; i < grid.length; i++) {
+		for (let j = 0; j < grid[i].length; j++) {
+			// '1'을 찾으면 island count +1 해주고 붙어있는 모든 땅을 0으로 바꾸는 방법
+			if (grid[i][j] === '1') {
+				islandCount++;
+				dfs(i, j);
+			}
+		}
+	}
+
+	return islandCount;
+};
+
+let grid = [
+	['0', '1', '0', '1', '1'],
+	['1', '0', '1', '0', '1'],
+	['1', '1', '0', '1', '1'],
+	['1', '1', '1', '1', '1'],
+];
+
+console.log(numIslands(grid));
