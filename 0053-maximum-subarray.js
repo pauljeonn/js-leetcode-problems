a423d025bf10daac4761da099e29b9ec23d54a5d
Paul Jeon <49374104+pauljeonn@users.noreply.github.com>
2022-01-25 09:13:22 +0900

    0053. Maximum Subarray 수정

--- 0053-maximum-subarray.js
+++ 0053-maximum-subarray.js
@@ -1,6 +1,6 @@
 var maxSubArray = function (nums) {
 	// 숫자가 하나일 경우 그 숫자가 결과값이 된다.
-	let result = nums[0];
+	let max = nums[0];
 	// 처음 숫자가 음수면 최종합에서 제외시켜야된다.
 	let current = Math.max(max, 0);
 
@@ -8,12 +8,12 @@ var maxSubArray = function (nums) {
 	for (let i = 1; i < nums.length; i++) {
 		// 현재까지 합에 다음 숫자를 더해준다.
 		current += nums[i];
-		result = Math.max(max, current);
+		max = Math.max(max, current);
 		// 현재까지의 합이 음수면 최종합에 넣을 필요가 없으므로 current에 0을 할당한다
 		current = Math.max(current, 0);
 	}
 
-	return result;
+	return max;
 };
 
 console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
